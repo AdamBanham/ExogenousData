33180d5969a8d736608578dad9cd92936fb4d8a4
Adam Peter Banham <n7176546@qut.edu.au>
2021-10-01 18:46:02 +1000

    median looks at windows and takes a sample value for mid point
    However, the graphs are still very edgy. Good or Bad thing, not sure.

--- src/org/qut/exogenousaware/gui/workers/EnhancementMedianGraph.java
+++ src/org/qut/exogenousaware/gui/workers/EnhancementMedianGraph.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -20,14 +21,17 @@ import org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.plot.XYPlot;
 import org.jfree.chart.renderer.xy.DeviationRenderer;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataItem;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.jfree.data.xy.YIntervalSeries;
 import org.jfree.data.xy.YIntervalSeriesCollection;
+import org.qut.exogenousaware.ds.linear.BestFittingLine;
 import org.qut.exogenousaware.gui.panels.Colours;
 import org.qut.exogenousaware.gui.panels.ExogenousEnhancementDotPanel.GuardExpressionHandler;
 
@@ -53,9 +57,13 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
 	@Default Color failColour = new Color(128,0,0,255);
 	@Default Color nullColour = new Color(255,255,255,255);
 	@Default ChartPanel graph = null;
-	@Default double segmentInterval = 0.15;
+	@Default double segmentInterval = 0.05;
 	@Default @Getter JPanel main = new JPanel();
 	@Default JProgressBar progress = new JProgressBar();
+	@Default double lowerDomainBound = Double.MAX_VALUE;
+	@Default double upperDomainBound = Double.MIN_VALUE;
+	@Default double lowerRangeBound = Double.MAX_VALUE;
+	@Default double upperRangeBound = Double.MIN_VALUE;
 	
 	public EnhancementMedianGraph setup() {
 		this.main.setLayout(new BorderLayout(50,50));
@@ -75,14 +83,32 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
 		int seriescount = 0;
 		for(XYSeries series: (List<XYSeries>) this.graphData.getSeries()) {
 			Map<Double, List<Double>> medians = chooseMap(seriescount, trueMedians, falseMedians, nullMedians);
-			for(int i=0;i < series.getItemCount();i++) {
-				double x = (double) series.getX(i);
-				x = x - (x % segmentInterval);
-				double y = (double) series.getY(i);				
-				if (!medians.containsKey(x)) {
-					medians.put(x, new ArrayList<Double>());
+			double startx = series.getX(0).doubleValue();
+			startx = startx + (startx % this.segmentInterval);
+			double endx = series.getX(series.getItemCount()-1).doubleValue();
+			endx = endx + (endx % this.segmentInterval);
+			System.out.println("["+title+"] start :: "+startx);
+			System.out.println("["+title+"] end :: "+endx);
+//			sample between start and end using the window segment
+			while (startx <= endx) {
+				double sampleMiddle = startx + (this.segmentInterval/2.0);
+				sampleMiddle = sampleMiddle - (sampleMiddle % (this.segmentInterval/2.0) );
+				double sample = 0.0;
+				boolean foundsample = false;
+				try {
+					sample = this.findSample(series, startx, startx+this.segmentInterval);
+					foundsample = true;
+				} catch (Exception e) {
+//					System.out.println("["+title+"] Unable to find sample in window :: "+startx+" to "+(startx+this.segmentInterval));
 				}
-				medians.get(x).add(y);
+				if(foundsample) {
+					if (!medians.containsKey(sampleMiddle)) {
+						medians.put(sampleMiddle, new ArrayList<Double>());
+					}
+					medians.get(sampleMiddle).add(sample);
+					System.out.println("["+title+"] add point ("+sampleMiddle+", "+sample+")");
+				}
+				startx += this.segmentInterval;
 			}
 			seriescount++;
 			this.progress.setValue(seriescount);
@@ -150,6 +176,13 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
         plot.setRangeGridlinesVisible(false);
         plot.setDomainGridlinesVisible(false);
         plot.setBackgroundPaint(Colours.CHART_BACKGROUND);
+//      setup a smart value and domain range of interest  
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setLowerBound(this.lowerDomainBound);
+        axis.setUpperBound(this.upperDomainBound);
+        axis = plot.getRangeAxis();
+        axis.setLowerBound(45.0); // should be this.lowerRangeBound
+        axis.setUpperBound(205.0); // should be this.upperRangeBound
 //		chart.removeLegend();
 //		remake the graph 
 		this.graph = new ChartPanel(
@@ -178,7 +211,7 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
 	}
 	
 	
-	static public void createMedianSeries(XYSeries series, Map<Double, List<Double>> data) {
+	public void createMedianSeries(XYSeries series, Map<Double, List<Double>> data) {
 		for(Entry<Double, List<Double>> entry : data.entrySet()) {
 			int middle =entry.getValue().size() / 2;
 			Object[] values = entry.getValue().toArray();
@@ -195,6 +228,84 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
 		}
 	}
 	
+	public double findSample(XYSeries series, Double sampleStart, Double sampleEnd) throws ArithmeticException,CloneNotSupportedException {
+//		find a pair such that LHS is the lostmost of the sample point (mid) and RHS is the rightmost of the sample point
+//		return a value for the mid point of the sample
+		double sampleMiddle = sampleStart + ((sampleEnd - sampleStart) / 2.0);
+		XYDataItem pairLeft = null;
+		XYDataItem pairRight = null;
+		List<XYDataItem> items = (List<XYDataItem>) series.createCopy(0, series.getItemCount()-1).getItems();
+//		find data points in sample window
+		List<XYDataItem> windowedItems = items.stream()
+				.filter(it -> it.getX().doubleValue() >= sampleStart)
+				.filter(it -> it.getX().doubleValue() <= sampleEnd)
+				.collect(Collectors.toList());
+//		case (1) no window items
+		if (windowedItems.size() > 0) {
+//			case (1)(a) we have some datapoints in sample window
+//			case (2) we only have data points to one side of the middle
+			List<XYDataItem> leftmostItems = windowedItems.stream()
+					.filter(it -> it.getX().doubleValue() < sampleMiddle)
+					.collect(Collectors.toList());
+			List<XYDataItem> rightmostItems = windowedItems.stream()
+					.filter(it -> it.getX().doubleValue() > sampleMiddle)
+					.collect(Collectors.toList());
+			if (leftmostItems.size()==0 || rightmostItems.size()==0) {
+//				case(2)(a) need to find either a leftmost or a rightmost to complete pair
+				if (leftmostItems.size() == 0) {
+//					case(2)(a)(i) find a possible left else throw
+					List<XYDataItem> possibleLeft = items.stream()
+							.filter(it -> it.getX().doubleValue() < sampleMiddle)
+							.collect(Collectors.toList());
+					if (possibleLeft.size() > 0) {
+						leftmostItems.add(possibleLeft.get(possibleLeft.size()));
+					} else {
+						
+						throw new ArithmeticException("Unable to find a leftmost element for this sample window.");
+					}
+				} else {
+//					case(2)(a)(ii) find a possible right else throw
+					List<XYDataItem> possibleRight = items.stream()
+							.filter(it -> it.getX().doubleValue() > sampleMiddle)
+							.collect(Collectors.toList());
+					if (possibleRight.size() > 0) {
+						rightmostItems.add(possibleRight.get(0));
+					} else {
+						throw new ArithmeticException("Unable to find a rightmost element for this sample window.");
+					}
+				}
+			}
+//			case(3) we can make a pair
+			pairLeft = leftmostItems.get(leftmostItems.size()-1);
+			pairRight = rightmostItems.get(0);
+		} else {
+//			case (1)(b) no data points within window, need to right leftmost and rightmost
+			List<XYDataItem> possibleLeft = items.stream()
+					.filter(it -> it.getX().doubleValue() < sampleMiddle)
+					.collect(Collectors.toList());
+			List<XYDataItem> possibleRight = items.stream()
+					.filter(it -> it.getX().doubleValue() > sampleMiddle)
+					.collect(Collectors.toList());
+//			case (4) only items to one side of middle
+			if (possibleLeft.size() == 0 || possibleRight.size() == 0) {
+				throw new ArithmeticException("Unable to find suitable pair witin sample window");
+			} else {
+				pairLeft = possibleLeft.get(possibleLeft.size()-1);
+				pairRight = possibleRight.get(0);
+			}
+		}
+//		calculate the best fitting line and find y at middle of sample window
+		BestFittingLine sampler = BestFittingLine.builder()
+				.X(pairLeft.getXValue())
+				.X(pairRight.getXValue())
+				.Y(pairLeft.getYValue())
+				.Y(pairRight.getYValue())
+				.build()
+				.findSlope()
+				.findIntercept();
+		return sampler.findY(sampleMiddle);
+	}
+	
 	public void createIntervalSeries(YIntervalSeries series, Map<Double, List<Double>> data, XYSeries medians) {
 		StandardDeviation std = new StandardDeviation();
 		int medianCounter = 0;
@@ -209,6 +320,18 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
 			stdValue = Math.sqrt(stdValue);
 			double median = (double) medians.getY(medianCounter);
 			series.add( (double) medians.getX(medianCounter), median, median-stdValue, median+stdValue);
+//			check bounds for given point
+			try {
+			double x = medians.getX(medianCounter).doubleValue();
+			double lowy = median-stdValue;
+			double highy = median+stdValue;
+			this.lowerDomainBound = x < this.lowerDomainBound ? x : this.lowerDomainBound;
+			this.upperDomainBound = x > this.upperDomainBound ? x : this.upperDomainBound;
+			this.lowerRangeBound = lowy < this.lowerRangeBound ? lowy : this.lowerRangeBound;
+			this.upperRangeBound = highy > this.upperRangeBound ? highy : this.upperRangeBound;
+			} catch (Exception e) {
+				System.out.println("["+title+"] Error in bound comparision :: "+e.getMessage());
+			}
 			medianCounter++;
 		}
 	}
@@ -238,6 +361,13 @@ public class EnhancementMedianGraph extends SwingWorker<JPanel, String> {
 	}
 
 	protected JPanel doInBackground() throws Exception {
-		return this.make();
+		JPanel work = null;
+		try {
+			work = this.make();
+		} catch (Exception e) {
+			System.out.println("["+title+"] failed to do work :: "+e.getLocalizedMessage());
+			System.out.println("["+title+"] "+e.getCause());
+		}
+		return work;
 	}
 }
