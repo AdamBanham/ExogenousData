d2b0d9da31e0ac7a8b0d470bf9f18c7d63225196
Adam Peter Banham <n7176546@qut.edu.au>
2021-12-07 14:31:30 +1000

    placeholders for new slicing options.

---
+++ src/org/processmining/qut/exogenousaware/steps/slicing/FutureOutcomeSlicer.java
@@ -0,0 +1,32 @@
+package org.processmining.qut.exogenousaware.steps.slicing;
+
+import java.util.Map;
+
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XTrace;
+import org.processmining.qut.exogenousaware.data.ExogenousDataset;
+import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries;
+
+public class FutureOutcomeSlicer implements Slicer {
+
+	public void createUserChoices() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getShortenName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Map<XEvent, SubSeries> slice(XTrace endogenous, XTrace exogenous, ExogenousDataset dataset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
