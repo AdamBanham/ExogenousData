76485b0fa1749bfd02dc9afcbfd97f094ac8775d
Adam Peter Banham <n7176546@qut.edu.au>
2021-12-07 14:26:19 +1000

    updated enum for dataset link type

--- src/org/processmining/qut/exogenousaware/data/ExogenousDatasetLinkType.java
+++ src/org/processmining/qut/exogenousaware/data/ExogenousDatasetLinkType.java
@@ -1,6 +1,19 @@
 package org.processmining.qut.exogenousaware.data;
 
+import org.deckfour.xes.model.XLog;
+
 public enum ExogenousDatasetLinkType {
-	TRACE_ATTRIBUTE_MATCH,
-	EVENT_ATTRIBUTE_MATCH;
+	TRACE_ATTRIBUTE_MATCH("match"),
+	EVENT_ATTRIBUTE_MATCH("ematch");
+	
+	private String id;
+	
+	private ExogenousDatasetLinkType(String id) {
+		this.id = id;
+	}
+	
+	public boolean checkLog(XLog log) {
+		return false;
+	}
+	
 }
