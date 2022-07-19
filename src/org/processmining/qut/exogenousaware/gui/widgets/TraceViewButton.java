5ce07fae8b182151644d3c37b272e02a9eafd42a
Adam Peter Banham <n7176546@qut.edu.au>
2022-07-19 17:25:28 +1000

    button does not resize on click.
    instead it now jumps to right after click. progress?

--- src/org/processmining/qut/exogenousaware/gui/widgets/TraceViewButton.java
+++ src/org/processmining/qut/exogenousaware/gui/widgets/TraceViewButton.java
@@ -1,6 +1,7 @@
 package org.processmining.qut.exogenousaware.gui.widgets;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import com.fluxicon.slickerbox.components.SlickerButton;
 
@@ -9,6 +10,7 @@ public class TraceViewButton extends SlickerButton{
 	Color ACTIVE = new Color(45,164,78,255);
 	Color UNACTIVE = Color.darkGray;
 	private boolean active = false;
+	Dimension preferred;
 	
 	public TraceViewButton(String text) {
 		super(text);
@@ -16,6 +18,9 @@ public class TraceViewButton extends SlickerButton{
 		this.COLOR_HILIGHT_MOUSEOVER = UNACTIVE;
 		this.COLOR_BG_MOUSEOVER = ACTIVE;
 		this.initialize();
+		int width = (this.width + 15);
+		int height = (this.height);
+		preferred = new Dimension(width, height);
 	}
 	
 	public void setActive(boolean active) {
@@ -26,13 +31,25 @@ public class TraceViewButton extends SlickerButton{
 			this.COLOR_BG_ACTIVE = ACTIVE;
 			this.COLOR_BG_DISABLED = ACTIVE;
 			this.COLOR_BG_FOCUS = ACTIVE;
+			this.width += 15;
 		} else {
 			this.COLOR_BG = UNACTIVE;
 			this.COLOR_BG_ACTIVE = UNACTIVE;
 			this.COLOR_BG_DISABLED = UNACTIVE;
 			this.COLOR_BG_FOCUS = UNACTIVE;
+			this.width -= 15;
 		}
 		this.initialize();
+	}
+	
+	@Override
+	public int getWidth() {
+		return (int) preferred.getWidth() -15;
+	}
+	
+	@Override
+	public int getHeight() {
+		return (int) preferred.getHeight();
 	}
 	
 	
