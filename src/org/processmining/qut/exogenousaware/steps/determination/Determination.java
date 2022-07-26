cd3441b99ca47d8805edd23262476bf04e622ce9
Adam Peter Banham <n7176546@qut.edu.au>
2022-07-26 11:29:51 +1000

    added apply step to generate exogenous linkage between endogenous traces

--- src/org/processmining/qut/exogenousaware/steps/determination/Determination.java
+++ src/org/processmining/qut/exogenousaware/steps/determination/Determination.java
@@ -1,8 +1,17 @@
 package org.processmining.qut.exogenousaware.steps.determination;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.deckfour.xes.model.XEvent;
+import org.deckfour.xes.model.XTrace;
 import org.processmining.qut.exogenousaware.data.ExogenousDataset;
 import org.processmining.qut.exogenousaware.steps.linking.Linker;
 import org.processmining.qut.exogenousaware.steps.slicing.Slicer;
+import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries;
+import org.processmining.qut.exogenousaware.steps.transform.data.TransformedAttribute;
 import org.processmining.qut.exogenousaware.steps.transform.type.Transformer;
 
 import lombok.AllArgsConstructor;
@@ -26,6 +35,33 @@ public class Determination {
 		name = name + slicer.getName() + " -> ";
 		name = name + transformer.getName();
 		return name;
+	}
+	
+	/**
+	 * Apply the internal steps of an determination to an endogenous trace. 
+	 * This may result in transformed attributes being added to events that
+	 * belong to the endogenous trace.
+	 * @param endo : the endogenous trace to be considered
+	 * @return A list of found subseries from this determination
+	 */
+	public List<SubSeries> apply(XTrace endo){
+		List<SubSeries> foundSubSeries = new ArrayList<>();
+//		perform the linkage
+		List<XTrace> exogenousSeries = linker.link(endo, panel.getSource());
+//		if we have some linkage, then perform slicing
+		for(XTrace exogenous: exogenousSeries) {
+			Map<XEvent, SubSeries> result = slicer.slice(endo, exogenous, panel);
+//			if we have some subseries, then perform transformation
+			for(Entry<XEvent, SubSeries> item: result.entrySet()) {
+				TransformedAttribute xattr = transformer.transform(item.getValue());
+				if (xattr != null) {
+					item.getKey().getAttributes()
+						.put(xattr.getKey(), xattr);
+				}
+				foundSubSeries.add(item.getValue());
+			}
+		}			
+		return foundSubSeries;
 	}
 
 }
