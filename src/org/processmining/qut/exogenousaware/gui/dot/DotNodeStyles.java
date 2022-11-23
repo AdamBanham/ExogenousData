e2f1551080cd767cbda53cfc14f809bc8998d6cb
Adam Peter Banham <n7176546@qut.edu.au>
2022-11-23 16:53:23 +1000

    updated dot styling for guards + measures

--- src/org/processmining/qut/exogenousaware/gui/dot/DotNodeStyles.java
+++ src/org/processmining/qut/exogenousaware/gui/dot/DotNodeStyles.java
@@ -16,6 +16,7 @@ import org.processmining.plugins.graphviz.dot.DotEdge;
 import org.processmining.plugins.graphviz.dot.DotNode;
 import org.processmining.qut.exogenousaware.data.dot.GuardExpressionHandler;
 import org.processmining.qut.exogenousaware.stats.models.ProcessModelStatistics;
+import org.processmining.qut.exogenousaware.stats.models.ProcessModelStatistics.DecisionPoint;
 
 /**
  * Static class for making exogenous dot nodes for a dot visusalisation.
@@ -149,61 +150,140 @@ public class DotNodeStyles {
 	
 	public static DotNode buildRuleTransition(Transition t, GuardExpression g, Map<String,String> swapper) {
 		boolean istau = t.getLabel().startsWith("tau ");
+		System.out.println("Not Deciding if we need recall...");
 		String label = createTransitionLabel(istau ? "" : t.getLabel(), istau, g, swapper);
 		return new ExoDotTransition(label,t.getId().toString(), t.getLabel(), new GuardExpressionHandler(g, swapper));
 	}
 	
 	public static DotNode buildRuleTransition(Transition t, ProcessModelStatistics stats, GuardExpression g, Map<String,String> swapper) {
 		boolean istau = t.getLabel().startsWith("tau ");
-		String label = createTransitionLabel(istau ? buildTauStatLabel(t, stats) : buildStatLabel(t, stats), istau, g, swapper);
+//		work out if we have a guard wise recall measurement
+		String recallKey = t.getId().toString()+"-recall";
+		String precisionKey = t.getId().toString()+"-precision";
+		DecisionPoint dp = stats.findDecision(t);
+		double recallMeasure = 0.0;
+		double precisionMeasure = 0.0;
+		if (dp != null) {
+			if (dp.getMapToMeasures().containsKey(recallKey)) {
+				recallMeasure = dp.getMapToMeasures().get(recallKey);
+			}
+			if (dp.getMapToMeasures().containsKey(precisionKey)) {
+				precisionMeasure = dp.getMapToMeasures().get(precisionKey);
+			}
+		}
+//		call the big function if recall is set
+		String label;
+		if (recallMeasure > 0.0 && precisionMeasure < 0.01) {
+			label = createTransitionLabel(istau ? buildTauStatLabel(t, stats) : buildStatLabel(t, stats), istau, g, swapper, recallMeasure, true);
+		} 
+		else if (recallMeasure < 0.01 && precisionMeasure > 0.00) {
+			label = createTransitionLabel(istau ? buildTauStatLabel(t, stats) : buildStatLabel(t, stats), istau, g, swapper, precisionMeasure, false);
+		}	
+		else if (recallMeasure > 0.00 && precisionMeasure > 0.00) {
+			label = createTransitionLabel(istau ? buildTauStatLabel(t, stats) : buildStatLabel(t, stats), istau, g, swapper, recallMeasure, precisionMeasure);
+		}
+		else {
+			label = createTransitionLabel(istau ? buildTauStatLabel(t, stats) : buildStatLabel(t, stats), istau, g, swapper);
+		}
 		return new ExoDotTransition(label,t.getId().toString(), t.getLabel(), new GuardExpressionHandler(g, swapper));
 	}
 	
 	private static String createTransitionLabel(String label, boolean istau) {
 		String labelFortmat = ""
-				+ "<<TABLE BGCOLOR=\"%s\" BORDER=\"1\"     CELLPADDING=\"0\" CELLSPACING=\"0\" PORT=\"HEAD\">"
+				+ "<<TABLE WIDTH=\"110\" COLUMNS=\"10\" BGCOLOR=\"%s\" BORDER=\"1\"  style=\"rounded\"  CELLPADDING=\"5\" CELLSPACING=\"5\" PORT=\"HEAD\">"
 				+ "<TR>"
-				+ "<TD BORDER=\"0\"   CELLPADDING=\"2\"   >"
+				+ "<TD colspan=\"10\" BORDER=\"0\" CELLPADDING=\"2\" ALIGN=\"CENTER\">"
 				+ "<FONT COLOR=\"%s\">%s </FONT>"
 				+ "</TD>"
 				+ "</TR>"
 				+ "<TR>"
-				+ "<TD BGCOLOR=\"black\" HEIGHT=\"5\"   ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
+				+ "<TD colspan=\"10\" BORDER=\"0\" HEIGHT=\"18\" ALIGN=\"CENTER\" VALIGN=\"MIDDLE\"><FONT COLOR=\"red\">(Guard-wise) Reasoning-Recall:</FONT></TD>"
 				+ "</TR><TR>"
-				+ "<TD BGCOLOR=\"black\" HEIGHT=\"5\"   ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
-				+ "</TR>";
+				+ "<TD colspan=\"10\" HEIGHT=\"10\" WIDTH=\"110\" BGCOLOR=\"gray\" style=\"rounded\"  ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
+				+ "</TR><TR>"
+				+ "<TD colspan=\"10\" BORDER=\"0\" HEIGHT=\"18\" ALIGN=\"CENTER\"><FONT COLOR=\"red\">(Guard-wise) Reasoning-Precision:</FONT></TD>"
+				+ "</TR><TR>"
+				+ "<TD colspan=\"10\" HEIGHT=\"10\" WIDTH=\"110\" BGCOLOR=\"gray\" style=\"rounded\"  ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
+				+ "</TR>"
+				+ "<TR><TD colspan=\"10\" style=\"rounded\" BGCOLOR=\"WHITE\"><TABLE BORDER=\"0\"><TR><TD BORDER=\"0\"><FONT COLOR=\"red\">Guard:</FONT></TD></TR>";
 		
 		String end = ""
-				+ "</TABLE>>";
+				+ "</TABLE></TD></TR></TABLE>>";
 		String formattedlabel = String.format(labelFortmat, 
 				istau ? "BLACK" : "WHITE",
 				istau ? "WHITE" : "BLACK",
 				istau ? "&tau;"+label : label
 		);
-		formattedlabel = formattedlabel + end;
+		String guard = "<TR><TD BGCOLOR=\"WHITE\" CELLPADDING=\"5\" ALIGN=\"CENTER\" BORDER=\"0\" style=\"rounded\"><FONT POINT-SIZE=\"8\" COLOR=\"BLACK\">TRUE</FONT></TD></TR>";
+		formattedlabel = formattedlabel + guard + end;
 		return formattedlabel;
 	}
 	
 	private static String createTransitionLabel(String label, boolean istau, GuardExpression g, Map<String,String> swapper) {
+		return createTransitionLabel(label, istau, g, swapper, 0.00f, 0.00f);
+	}
+	
+	private static String createTransitionLabel(String label, boolean istau, GuardExpression g, Map<String,String> swapper, double measure, boolean isrecall) {
+		if (isrecall) {
+			return createTransitionLabel(label, istau, g, swapper, measure, 0.01f);
+		}
+		return createTransitionLabel(label, istau, g, swapper, 0.01f, measure);
+	}
+
+	private static String createTransitionLabel(String label, boolean istau, GuardExpression g, Map<String,String> swapper, double recall, double precision) {
 		String labelFortmat = ""
-				+ "<<TABLE BGCOLOR=\"%s\" BORDER=\"1\"     CELLPADDING=\"0\" CELLSPACING=\"0\" PORT=\"HEAD\">"
+				+ "<<TABLE WIDTH=\"110\" COLUMNS=\"*\" ROWS=\"4\" BGCOLOR=\"%s\" BORDER=\"1\" style=\"rounded\" CELLPADDING=\"5\" CELLSPACING=\"5\" PORT=\"HEAD\">"
 				+ "<TR>"
-				+ "<TD BORDER=\"0\"   CELLPADDING=\"2\"   >"
-				+ "<FONT COLOR=\"%s\">%s </FONT>"
+				+ "<TD colspan=\"10\" WIDTH=\"110\" BORDER=\"0\" CELLPADDING=\"2\" ALIGN=\"CENTER\" VALIGN=\"MIDDLE\">"
+				+ "<FONT COLOR=\"%s\">%s</FONT>"
 				+ "</TD>"
 				+ "</TR>"
 				+ "<TR>"
-				+ "<TD BGCOLOR=\"black\" HEIGHT=\"5\"   ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
+				+ "<TD colspan=\"10\" BORDER=\"0\" HEIGHT=\"18\" ALIGN=\"CENTER\" VALIGN=\"MIDDLE\"><FONT COLOR=\"red\">(Guard-wise) Reasoning-Recall: %s</FONT></TD>"
 				+ "</TR><TR>"
-				+ "<TD BGCOLOR=\"black\" HEIGHT=\"5\"   ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
-				+ "</TR>";
+				+ "<TD colspan=\"10\" HEIGHT=\"10\" WIDTH=\"110\" BGCOLOR=\"%s\" style=\"rounded\"  ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
+				+ "</TR><TR>"
+				+ "<TD colspan=\"10\" BORDER=\"0\" HEIGHT=\"18\" ALIGN=\"CENTER\"><FONT COLOR=\"red\">(Guard-wise) Reasoning-Precision: %s</FONT></TD>"
+				+ "</TR><TR>"
+				+ "<TD colspan=\"10\" HEIGHT=\"10\" WIDTH=\"110\" BGCOLOR=\"%s\" style=\"rounded\"  ALIGN=\"LEFT\" BORDER=\"0\" CELLPADDING=\"2\"   CELLSPACING=\"0\"></TD>"
+				+ "</TR>"
+				+ "<TR><TD COLSPAN=\"10\" style=\"rounded\" BGCOLOR=\"WHITE\"><TABLE BORDER=\"0\"><TR><TD BORDER=\"0\"><FONT COLOR=\"red\">Guard:</FONT></TD></TR>";
 		
 		String end = ""
-				+ "</TABLE>>";
+				+ "</TABLE></TD></TR></TABLE>>";
+		
+		String precisionMeasure;
+		String precisionBarColoured = "green;%.2f:gray";
+		String precisionBarUnColoured = "gray";
+		String precisionBar = "";
+		if (precision > 0.01) {
+			precisionBar = String.format(precisionBarColoured, precision);
+			precisionMeasure = String.format("%.2f%%", precision * 100.0);
+		} else {
+			precisionBar = precisionBarUnColoured;
+			precisionMeasure = "";
+		}
+		
+		String recallMeasure;
+		String recallBarColoured = "gold;%.2f:gray";
+		String recallBarUnColoured = "gray";
+		String recallBar = "";
+		if (recall > 0.01) {
+			recallBar = String.format(recallBarColoured, recall);
+			recallMeasure = String.format("%.2f%%", recall * 100.0);
+		} else {
+			recallBar = recallBarUnColoured;
+			recallMeasure = "";
+		}
+		
 		String formattedlabel = String.format(labelFortmat, 
 				istau ? "BLACK" : "WHITE",
 				istau ? "WHITE" : "BLACK",
-				istau ? "&tau;"+label : label
+				istau ? "&tau;"+label : label,
+				recallMeasure,
+				recallBar,
+				precisionMeasure,
+				precisionBar
 		);
 		List<String> exprList = new ArrayList<String>();
 		try {
@@ -249,9 +329,7 @@ public class DotNodeStyles {
 	}
 	
 	private static String formatExpression(String expr, int key, String prefix, Map<String,String> swapper) {
-		String ruleFormat = "<TR>"
-				+ "<TD BGCOLOR=\"WHITE\" CELLPADDING=\"5\" ALIGN=\"LEFT\" BORDER=\"0\"><FONT COLOR=\"RED\" POINT-SIZE=\"9\">[%s%d]</FONT><FONT POINT-SIZE=\"8\" COLOR=\"BLACK\"> %s </FONT> </TD>"
-				+ "</TR>";
+		String ruleFormat = "<TR><TD BGCOLOR=\"WHITE\" CELLPADDING=\"5\" ALIGN=\"LEFT\" BORDER=\"0\" style=\"rounded\"><FONT COLOR=\"RED\" POINT-SIZE=\"9\">[%s%d]</FONT><FONT POINT-SIZE=\"8\" COLOR=\"BLACK\"> %s </FONT> </TD></TR>";
 		for (Entry<String, String> val : swapper.entrySet()) {
 			expr = expr.replace(val.getValue(), val.getKey());
 		}
@@ -262,9 +340,7 @@ public class DotNodeStyles {
 	}
 	
 	private static String formatExpression(String expr, int key, Map<String,String> swapper) {
-		String ruleFormat = "<TR>"
-				+ "<TD BGCOLOR=\"WHITE\" CELLPADDING=\"5\" BORDER=\"1\"><FONT COLOR=\"RED\" POINT-SIZE=\"9\">[R%d]</FONT><FONT POINT-SIZE=\"8\" COLOR=\"BLACK\"> %s </FONT> </TD>"
-				+ "</TR>";
+		String ruleFormat = "<TR><TD BGCOLOR=\"WHITE\" style=\"rounded\" CELLPADDING=\"5\" BORDER=\"1\"><FONT COLOR=\"RED\" POINT-SIZE=\"9\">[R%d]</FONT><FONT POINT-SIZE=\"8\" COLOR=\"BLACK\"> %s </FONT> </TD></TR>";
 		for (Entry<String, String> val : swapper.entrySet()) {
 			expr = expr.replace(val.getValue(), val.getKey());
 		}
@@ -276,7 +352,7 @@ public class DotNodeStyles {
 	
 	private static String createTableRow(List<String> exprs, int key, Map<String,String> swapper) {
 		List<String> rows = new ArrayList<String>();
-		String tableFormat = "<TR><TD><TABLE BORDER=\"1\"><TR><TD CELLPADDING=\"5\" ALIGN=\"LEFT\" BORDER=\"0\"><FONT COLOR=\"RED\" POINT-SIZE=\"9\">[R%d]</FONT>"
+		String tableFormat = "<TR><TD style=\"rounded\"  CELLPADDING=\"1\" BORDER=\"1\"><TABLE BORDER=\"0\"><TR><TD CELLPADDING=\"5\" ALIGN=\"LEFT\" BORDER=\"0\"><FONT COLOR=\"RED\" POINT-SIZE=\"9\">[R%d]</FONT>"
 				+ "<FONT POINT-SIZE=\"9\" COLOR=\"BLACK\"> - Requires the following to be true: </FONT>"
 				+ "</TD></TR>";
 		tableFormat = String.format(tableFormat, key+1);
