983c7ef7a42072c56419f530395b57a780f5d80b
Adam Peter Banham <n7176546@qut.edu.au>
2022-12-07 17:49:45 +1000

    linting.

--- src/org/processmining/qut/exogenousaware/measures/datapetrinets/DecisionRecall.java
+++ src/org/processmining/qut/exogenousaware/measures/datapetrinets/DecisionRecall.java
@@ -37,63 +37,71 @@ import lombok.NonNull;
 
 @Builder
 public class DecisionRecall implements PetriNetMeasure {
-	
+//	required parameters
 	@NonNull Integer progressInc;
 	@NonNull private ExogenousDiscoveryProgresser progresser;
 	@NonNull Map<String,String> variableMapping;
 
-
+//  static references for measure
 	public static String NAME = "decision-recall";
 	
 	/**
-	 * Computes decision-recall for the given log, model and alignment, <br>
-	 * whereby, the measure returns 1.0, when for each observation, we could have
-	 *  evaluated the described guard of a choice, and <br>
-	 * otherwise returns 0.0 when for each observation, we couldn't evaluate the
-	 *  described guard or the guard was a truth.
+	 * Computes decision-recall for the given log, model and alignment,
+	 * whereby, the measure returns 1.0, when for each observation, we could have 
+	 * evaluated the described guard of a choice, and otherwise returns 0.0 when 
+	 * for each observation, we couldn't evaluate the
+	 * described guard or the guard was a truth.
 	 */
-	public double measure(XLog log, Object model, ModelStatistics statistics, Object alignment) {
+	public double measure(XLog log, Object model, ModelStatistics statistics, 
+			Object alignment) {
 		ProgressState state = progresser.getState(ProgressType.Measurements);
-		
-		
 		double measure = 0.0;
 //		check for the right type of alignment result
 		PNRepResult alignmentResult;
 		if (alignment instanceof PNRepResult) {
 			alignmentResult = (PNRepResult) alignment;
 		} else {
-			throw new IllegalArgumentException("Unknown alignment result of :: "+ alignment.getClass().toGenericString());
+			throw new IllegalArgumentException("Unknown alignment result of :: "
+					+ alignment.getClass().toGenericString());
 		}
-		
 //		check for the right type of modelstatistics
 		ProcessModelStatistics statisticResult;
 		if (statistics instanceof ProcessModelStatistics) {
 			statisticResult = (ProcessModelStatistics) statistics;
 		} else {
-			throw new IllegalArgumentException("Unknown statistics result of :: "+ statistics.getClass().toGenericString());
+			throw new IllegalArgumentException("Unknown statistics result of :: "
+					+ statistics.getClass().toGenericString());
 		}
-		
 //		check for the right type of model 
 		PetriNetWithData modeller;
 		if (model instanceof PetriNetWithData) {
 			modeller = (PetriNetWithData) model;
 		}
-		
 		double totalrfsum = 0.0;
 //		compute total measure but store local measures in statistics
 		for( Place dplace : statisticResult.getDecisionMoments()) {
-			double decisionfreq = statisticResult.getInformation(dplace).getRelativeFrequency();
+			double decisionfreq = statisticResult.getInformation(dplace)
+					.getRelativeFrequency();
 			double localmeasure = 0.0;
 			double localrfsum = 0.0;
-			List<Observation> obs = getObservations(log, alignmentResult, dplace, statisticResult);
+			List<Observation> obs = getObservations(log, alignmentResult, dplace,
+					statisticResult);
+//			for each outcome of this choice, compute the local measure
 			for(Transition outcome : statisticResult.getDecisionOutcomes(dplace)) {
 				double outcomemeasure = 0.0;
 				double outcomefreq = 0.0;
-				if (statisticResult.getInformation(dplace).getMapToFrequency().containsKey(outcome)) {
-					outcomefreq = statisticResult.getInformation(dplace).getMapToFrequency().get(outcome);
+//				get relative frequency of outcome
+				if (statisticResult.getInformation(dplace).getMapToFrequency()
+						.containsKey(outcome)) {
+					outcomefreq = statisticResult.getInformation(dplace)
+							.getMapToFrequency().get(outcome);
 				}
 				final double freq = outcomefreq * decisionfreq;
-				List<Observation> outcomeObs = obs.stream().filter(o -> o.getOutcome().equals(outcome)).collect(Collectors.toList());
+//				find all observations for this outcome
+				List<Observation> outcomeObs = obs.stream()
+						.filter(o -> o.getOutcome().equals(outcome))
+						.collect(Collectors.toList());
+//				hard coded for petri net with data handling
 				if (outcome instanceof PNWDTransition) {
 					PNWDTransition trans = (PNWDTransition) outcome;
 					Optional<Double> totalOutcomeRF = null;
@@ -110,24 +118,29 @@ public class DecisionRecall implements PetriNetMeasure {
 						}
 						outcomemeasure += measurerf;
 						double guardwise = (measurerf/(freq * outcomeObs.size())) ;
-						statisticResult.getInformation(dplace).addMeasure(outcome.getId().toString()+"-recall", guardwise);
+						statisticResult.getInformation(dplace)
+						.addMeasure(outcome.getId().toString()+"-recall", guardwise);
 					}
 					localrfsum += freq * outcomeObs.size();
 				}
 //				add to decision point's measure
 				localmeasure += outcomemeasure;
 				state.increment(progressInc);
-				System.out.println("[DecisionRecall] outcome ("+outcome.getLabel()+") decision-recall for "+ outcome.getLabel().toLowerCase() + " was :: "+ outcomemeasure+ "/"+ freq * outcomeObs.size());
+				System.out.println("[DecisionRecall] outcome ("+outcome.getLabel()
+					+") decision-recall for "+ outcome.getLabel().toLowerCase() 
+					+" was :: "+ outcomemeasure+ "/"+ freq * outcomeObs.size());
 			}
-			statisticResult.addMeasureToDecisionMoment(dplace, NAME, (localmeasure/localrfsum));
+			statisticResult.addMeasureToDecisionMoment(dplace, NAME, 
+					(localmeasure/localrfsum));
 			totalrfsum += localrfsum;
-			System.out.println("[DecisionRecall] local decision-recall for "+ dplace.getLabel().toLowerCase() + " was :: "+localmeasure+"/"+localrfsum);
+			System.out.println("[DecisionRecall] local decision-recall for "
+					+ dplace.getLabel().toLowerCase() + " was :: "
+					+ localmeasure+"/"+localrfsum);
 			measure += localmeasure;
 		}
-		System.out.println("[DecisionRecall] computed decision-recall was :: "+ measure+"/"+totalrfsum);
+		System.out.println("[DecisionRecall] computed decision-recall was :: "
+				+ measure+"/"+totalrfsum);
 		return (measure/totalrfsum);
-		
-		
 	}
 	
 	/**
@@ -138,7 +151,6 @@ public class DecisionRecall implements PetriNetMeasure {
 	 */
 	private Boolean isExpressionSimpleTruth(GuardExpression expr) {
 		boolean test = true;
-		
 		if (expr == null) {
 			return true;
 		}
@@ -147,7 +159,8 @@ public class DecisionRecall implements PetriNetMeasure {
 	}
 	
 	/**
-	 * Tests that for a observation we have a datastate with variables within expr, thus proving that they are related.
+	 * Tests that for a observation we have a datastate with variables within expr,
+	 *  thus proving that they are related.
 	 * @param obs
 	 * @param expr
 	 * @return do we have all variables for expr
@@ -173,19 +186,17 @@ public class DecisionRecall implements PetriNetMeasure {
 		}
 	}
 	
-	private List<Observation> getObservations(XLog log, PNRepResult alignments, Place decisionPoint, ProcessModelStatistics stats){
+	private List<Observation> getObservations(XLog log, PNRepResult alignments,
+			Place decisionPoint, ProcessModelStatistics stats){
 		List<Observation> obs = new ArrayList();
-		
 		for(SyncReplayResult alignment  : alignments) {
 //			walk alignment and count when needed
-			
 			int partialTracePoint = -1;
 			double missteps = 0;
 			double steps = 0;
 			for(int i=0; i < alignment.getNodeInstance().size(); i++) {
-				
+//				keep track of trace index via step type.
 				StepTypes step = alignment.getStepTypes().get(i);
-				
 				if (step == StepTypes.L || step == StepTypes.LMGOOD) {
 					partialTracePoint++;
 				}
@@ -194,19 +205,22 @@ public class DecisionRecall implements PetriNetMeasure {
 					missteps += 1;
 				}
 				steps += 1;
-				
-//				(1) search for decision outcome node, such that it was correctly aligned or routing
+//				(1) search for decision outcome node, such that it was correctly 
+//				    aligned or routing
 				Object node = alignment.getNodeInstance().get(i);
 				if (stats.getDecisionOutcomes(decisionPoint).contains(node)) {
-					if ( step == StepTypes.LMGOOD || alignment.getStepTypes().get(i) == StepTypes.MINVI) {
-//						(2) if it is a decision node outcome, compute observation data state
+					if ( step == StepTypes.LMGOOD 
+						 || alignment.getStepTypes().get(i) == StepTypes.MINVI
+						) {
+//						(2) if it is a decision node outcome, 
+//						    compute observation data state
 						for(int traceIndex : alignment.getTraceIndex()) {
 							
 							List<XEvent> partial = new ArrayList();
 							if (partialTracePoint > -1) {
-								partial = log.get(traceIndex).subList(0, partialTracePoint+1);
+								partial = log.get(traceIndex).subList(0, 
+										partialTracePoint+1);
 							}
-//							System.out.println("[DecisionRecall] Computed likelihood is :: "+(1.0f - (missteps/steps)));
 							obs.add(Observation.builder()
 								.outcome((Transition) node)
 								.partial(partial)
@@ -216,13 +230,10 @@ public class DecisionRecall implements PetriNetMeasure {
 								.setup()
 								);
 						}
-					
 					}
 				}
-				
 			}
 		}
-		
 		return obs;
 	}
 	
@@ -240,7 +251,6 @@ public class DecisionRecall implements PetriNetMeasure {
 		
 		public Observation setup() {
 //			build data state
-			
 			List<XEvent> preEvents = new ArrayList();
 			if (partial.size() > 1) {
 				preEvents = partial.subList(0, partial.size()-1);
@@ -263,13 +273,12 @@ public class DecisionRecall implements PetriNetMeasure {
 					processValue(key, datastate, attr.getValue());
 				}
 			}
-			
-//			System.out.println("Final pre-transition state :: "+datastate.toString());
-			
 //			create post state
 			postDatastate.putAll(datastate);
 			if (postEvent != null) {
-				for ( Entry<String, XAttribute> attr: postEvent.getAttributes().entrySet()) {
+				for ( Entry<String, XAttribute> attr: postEvent.getAttributes()
+						.entrySet()
+					) {
 					String key;
 					
 					if (variableMapping.containsKey(attr.getKey())) {
@@ -281,11 +290,11 @@ public class DecisionRecall implements PetriNetMeasure {
 					processValue(key, postDatastate, attr.getValue());
 				}
 			}
-			
 			return this;
 		}
 		
-		private void processValue(String key, Map<String,Object> mapper, Object value) {
+		private void processValue(String key, Map<String,Object> mapper, 
+				Object value) {
 			Object realValue = null;
 			
 			if (value instanceof XAttributeContinuous) {
@@ -297,12 +306,7 @@ public class DecisionRecall implements PetriNetMeasure {
 			} else {
 				realValue = value.toString();
 			}
-//			System.out.println("Addding :: ("+key+","+realValue+")");
 			mapper.put(key,realValue);
 		}
-		
 	}
-	
-	
-
 }
