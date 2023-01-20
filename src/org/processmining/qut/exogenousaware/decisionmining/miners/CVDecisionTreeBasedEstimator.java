e6e0eecd3a6c83913a2d0d475d4a1ab2a488491d
Adam Peter Banham <n7176546@qut.edu.au>
2023-01-20 17:07:33 +1000

    Update CVDecisionTreeBasedEstimator.java

--- src/org/processmining/qut/exogenousaware/decisionmining/miners/CVDecisionTreeBasedEstimator.java
+++ src/org/processmining/qut/exogenousaware/decisionmining/miners/CVDecisionTreeBasedEstimator.java
@@ -1,18 +1,16 @@
 package org.processmining.qut.exogenousaware.decisionmining.miners;
 
-import java.util.ArrayList;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-import org.processmining.datadiscovery.estimators.AbstractDecisionTreeFunctionEstimator;
 import org.processmining.datadiscovery.estimators.DecisionTreeBasedFunctionEstimator;
 import org.processmining.datadiscovery.estimators.FunctionEstimation;
 import org.processmining.datadiscovery.estimators.Type;
+import org.processmining.datadiscovery.estimators.impl.DecisionTreeFunctionEstimator;
 
-import weka.classifiers.AbstractClassifier;
-import weka.core.Attribute;
 import weka.core.Instances;
 
 /*
@@ -24,24 +22,33 @@ import weka.core.Instances;
  * See org.processmining.datadiscovery.estimators.impl.OverlappingEstimatorLocalDecisionTree;
  */
 public class CVDecisionTreeBasedEstimator<T extends DecisionTreeBasedFunctionEstimator> 
-	extends AbstractDecisionTreeFunctionEstimator {
+	extends DecisionTreeFunctionEstimator {
 	
     //states
+	protected T dummyEstimator = null;
 	protected T bestEstimator = null;
 	protected int cvFolds = 5;
-	protected Instances data;
 	
 	public CVDecisionTreeBasedEstimator(Map<String, Type> attributeType,
 			Map<String, Set<String>> literalValues, Object[] outputClasses, int capacity, String name) {
-		super(name, attributeType, literalValues, outputClasses, capacity);
-		
+		super(attributeType, literalValues, outputClasses, name, capacity);
+		try {
+			Class[] clazzes = new Class[5];
+			dummyEstimator = (T) dummyEstimator.getClass()
+					.getConstructor()
+					.newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public Map<Object, FunctionEstimation> getFunctionEstimation(Object[] option) throws Exception {
 		Map<Object, FunctionEstimation> retValue = new HashMap();
 		// create folds
 		// Make a copy of the data we can reorder
-		Instances data = new Instances(this.data);
+		Instances data = new Instances(this.instances);
 	    data.randomize(new Random());
 	    if (data.classAttribute().isNominal()) {
 	      data.stratify(cvFolds);
@@ -55,32 +62,6 @@ public class CVDecisionTreeBasedEstimator<T extends DecisionTreeBasedFunctionEst
 		}
 		
 		return retValue;
-	}
-
-	public void addInstance(Map<String, Object> attributes, Object classObject, float weight) throws Exception {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public Object classifyInstance(Map<String, Object> attributes) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public double computeQualityMeasure() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	protected ArrayList<Attribute> createAttributeList(Map<String, Type> attributeType,
-			Map<String, Set<String>> literalValues, Object[] outputClasses) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	protected AbstractClassifier createClassifier(Object[] option, boolean saveData) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
 	}
 	
 }
