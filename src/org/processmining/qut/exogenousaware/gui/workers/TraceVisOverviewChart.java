94a1f5d7539735db1b74c999f1646fd10409f5f3
Adam Peter Banham <n7176546@qut.edu.au>
2023-02-16 15:43:14 +1000

    updated overview impl to use time series representations.

--- src/org/processmining/qut/exogenousaware/gui/workers/TraceVisOverviewChart.java
+++ src/org/processmining/qut/exogenousaware/gui/workers/TraceVisOverviewChart.java
@@ -17,7 +17,6 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.stream.Collectors;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -39,14 +38,20 @@ import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 import org.processmining.qut.exogenousaware.data.ExogenousDatasetType;
+import org.processmining.qut.exogenousaware.ds.timeseries.approx.TimeSeriesSaxApproximator;
+import org.processmining.qut.exogenousaware.ds.timeseries.data.DiscreteTimeSeries;
+import org.processmining.qut.exogenousaware.ds.timeseries.data.RealTimePoint;
+import org.processmining.qut.exogenousaware.ds.timeseries.data.RealTimeSeries;
+import org.processmining.qut.exogenousaware.ds.timeseries.norm.TimeSeriesGuassianNormaliser;
+import org.processmining.qut.exogenousaware.ds.timeseries.reduce.PiecewiseAggregateReduction;
 import org.processmining.qut.exogenousaware.gui.ExogenousTraceView;
 import org.processmining.qut.exogenousaware.gui.colours.ColourScheme;
 import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries;
+import org.processmining.qut.exogenousaware.steps.slicing.data.SubSeries.Scaling;
 import org.processmining.qut.exogenousaware.steps.transform.data.TransformedAttribute;
 
 import lombok.Builder;
 import lombok.Builder.Default;
-import lombok.Getter;
 import lombok.NonNull;
 
 @Builder
@@ -64,7 +69,7 @@ public class TraceVisOverviewChart extends SwingWorker<JPanel, String> {
 	@Default double innerSpacing = 0.05;
 	
 //	SAX Gaussian equiprobable regions (10)
-	@Default List<Double> SAX_BOUNDARIES = new ArrayList<Double>() {{
+	@Default private List<Double> SAX_BOUNDARIES = new ArrayList<Double>() {{
 		add(-1.28);
 		add(-0.84);
 		add(-0.52);
@@ -75,7 +80,7 @@ public class TraceVisOverviewChart extends SwingWorker<JPanel, String> {
 		add(0.84);
 		add(1.28);
 	}};
-	@Default List<String> SAX_LETTERS = new ArrayList<String>() {{
+	@Default private List<String> SAX_LETTERS = new ArrayList<String>() {{
 		add("a");
 		add("b");
 		add("c");
@@ -160,7 +165,6 @@ public class TraceVisOverviewChart extends SwingWorker<JPanel, String> {
 //		for each attribute, find subseries
 //		then find set of exogenous time series to show
 		Set<XTrace> exogenousTimeSeries = new HashSet<XTrace>();
-		List<XTrace> exoSeries = new ArrayList();
 		List<SubSeries> slices = new ArrayList();
 		for(XEvent ev: this.endo) {
 			for(Entry<String, XAttribute> entry : ev.getAttributes().entrySet()) {
@@ -171,174 +175,76 @@ public class TraceVisOverviewChart extends SwingWorker<JPanel, String> {
 						boolean added = exogenousTimeSeries.add(xattr.getSource().getSource());
 						if (added) {
 							slices.add(xattr.getSource());
-							exoSeries.add(xattr.getSource().getSource());
 						}
 					}
 				}
 			}
 		}
-//		handle each series, by creating an x and y for each trace
-//		x being the timestamp
-//		y being the measurement (exogenous:value)
-//		keep track of min and max to make event slices
-		
+//		plot each source time series representation of slices
 		int exoCount = 0;
-		for(XTrace exo: exoSeries) {
-			List<Double> x = exo.stream().map((ev) -> getEventTimestampMillis(ev)).collect(Collectors.toList());
-			List<Double> y = exo.stream().map((ev) -> getExoEventValue(ev)).collect(Collectors.toList());
-			//	check if we have set up bounds before
-			double mean = 0.0;
-			double std = 0.0;
+		for(SubSeries exo: slices) {
+			RealTimeSeries series = exo.getSourceTimeSeries(Scaling.hour);
+//			check if we have set up bounds before
+			double mean = series.computeWeightedMean();
+			double std = series.computeStandardDeviation();
 			// perform normalisation to show time series differences?
 			if (normalize) {
-				Normalisation norm = normalise(y, x);
-				if (norm != null) {
-					y = norm.getValues();
-					mean = norm.getMean();
-					std = norm.getStd();
-				} else {
-					mean = y.stream().reduce((c,n) -> c + n).get() / y.size();
-					final double mmean = mean;
-					std = Math.sqrt(y.stream().reduce((c,n) -> c + Math.pow(n - mmean,2)).get() / y.size());
-				}
-			}
-			// prepare suitable min and max view range for plot
-			double min;
-			double high;
-			if (normalize) {
-				min = -3;
-				high = 3;
-			} else {
-				min = y.stream().reduce((c,n) -> c > n ? n : c).get();
-				high = y.stream().reduce((c,n) -> c < n ? n : c).get();
+				TimeSeriesGuassianNormaliser norm = new TimeSeriesGuassianNormaliser();
+				series = norm.normalise(series);
 			}
 			// perform dimensionality reduction via PAA?
 			if (ppa) {
-				y = PiecewiseAggregateAproximation(y);
-				x = PiecewiseAggregateAproximation(x);
+				PiecewiseAggregateReduction reducer = new PiecewiseAggregateReduction(100);
+				series = reducer.reduce(series);
 			}
+//			log out sax representation if selected
+			if (normalize && sax) {
+				TimeSeriesSaxApproximator approximator = new TimeSeriesSaxApproximator();
+				DiscreteTimeSeries dseries = approximator.approximate(series);
+				System.out.println("[TraceVisOverviewChart] sax representation of '"
+						+ series.getName()
+						+ "' is "
+						+ dseries.getValues()
+				);
+			}
+			// prepare suitable min and max view range for plot
+			double min = series.min();
+			double max = series.max();
 			if (!boundSet) {
 				bounds.put("low", min);
-				bounds.put("high", high);
+				bounds.put("high", max);
 				boundSet = true;
 			} else {
 //				check min and max for series against current bounds
 				if (bounds.get("low") > min) {
 					bounds.put("low", min);
 				}
-				if(bounds.get("high") < high) {
-					bounds.put("high", high);
+				if(bounds.get("high") < max) {
+					bounds.put("high", max);
 				}
 			}
 //			build series and render
 			String title = "%s (Exo-Panel#%d)";
-			String name = getExogenousName(exo);
-			XYSeries series;
+			String name = series.getName();
+			XYSeries xyseries;
 			if (normalize) {
-				title = title + " (%.2f/%.2f)";
-				series = new XYSeries(String.format(title, name, exoCount+1, mean, std));
+				title = title + " (M:%.1f/S.D.:%.1f)";
+				xyseries = new XYSeries(String.format(title, name, exoCount+1, mean, std));
 			} else {
-				series = new XYSeries(String.format(title, name, exoCount+1));
+				xyseries = new XYSeries(String.format(title, name, exoCount+1));
 			}
-			 
-			for(int idx = 0; idx < x.size(); idx++) {
-				series.add(x.get(idx) - starting, y.get(idx));
+//			 add time points
+			for(RealTimePoint point: series.getPoints()) {
+				xyseries.add(point.getTime(), point.getValue());
 			}
-			exoSignals.addSeries(series);
+			exoSignals.addSeries(xyseries);
 			exoRender.setSeriesShape(exoCount, new Ellipse2D.Double(-1.5,-1.5,3,3));
 			exoRender.setSeriesFillPaint(exoCount, Color.white);
-			exoRender.setSeriesPaint(exoCount, slices.get(exoCount).getComesFrom().getColourBase());
+			exoRender.setSeriesPaint(exoCount, series.getColor());
 			exoCount++;
 		}
 		
 		return seriesRenderBounds;
-	}
-	
-	private List<Double> PiecewiseAggregateAproximation(List<Double> series) {
-		 return PiecewiseAggregateAproximation(series, 100);
-	}
-	
-	private List<Double> PiecewiseAggregateAproximation(List<Double> series, int windows) {
-		if (series.size() < windows) {
-			System.out.println(String.format(
-					"[TraceVisOverviewChart] PPA window reduced from %d to...",
-					windows)
-			);
-			windows = (int)(series.size() * 0.05);
-			System.out.println("[TraceVisOverviewChart] ..."+windows);
-		}
-		List<Double> ret = new ArrayList<Double>(windows);
-		// perform aggregation
-		// c^_i = w/n * sum(c_j)
-		for(int i=1;i <= windows; i++) {
-			int start = (series.size()/ windows) * (i - 1) + 1;
-			int end = (series.size()/windows) * i;
-			List<Double> frame = series.subList(start, end);
-			
-			if (frame.size() == 1) {
-				ret.add(frame.get(0));
-			} else if (frame.size() < 1) {
-				continue;
-			} else {
-				double mean = frame.stream().reduce((c, n) -> c + n).get() / frame.size();
-				ret.add(mean);
-			}
-		}
-		
-		if (ret.size() != windows) {
-			System.out.println(String.format(
-						"[TraceVisOverviewChart] PPA stream returned different size :: "
-						+ "expected %d but saw %d.",
-						windows, ret.size())
-			);
-		}
-		
-		return ret;
-	}
-
-	@Builder
-	private static class Normalisation {
-		@Getter @NonNull List<Double> values;
-		@Getter double mean;
-		@Getter double std;
-	}
-	
-	private Normalisation normalise(List<Double> values, List<Double> times) {
-		if (values.size() < 2) {
-			return null;
-		}
-		
-		List<Double> ret = new ArrayList<Double>(values.size());
-		// perform weighted mean, calc using time different between now and last
-		double len = times.get(times.size() - 1) - times.get(0);
-		double durtemp = 0.0;
-		double tmp = 0.0;
-		for(int i=1; i < values.size(); i++) {
-			double dur = (times.get(i) - times.get(i -1)) / len;
-			double cval = values.get(i);
-			tmp += cval * dur;
-			durtemp += dur;
-		}
-		final double mean = tmp;
-		// find standard deviation
-		double std = values.stream()
-				.map( v -> v - mean)
-				.map( v -> Math.pow(v, 2))
-				.reduce((c,n) -> c + n)
-				.get();
-		std = std / values.size();
-		std = Math.sqrt(std);
-		// modify values and return new series
-		for(int i=0; i < values.size(); i++) {
-			double val = values.get(i);
-			val = (val - mean) / std;
-			ret.add(val);
-		}
-		return Normalisation.builder()
-				.values(ret)
-				.mean(mean)
-				.std(std)
-				.build();
 	}
 	
 	protected List<Object> buildSAXBoundaries(double left, double right){
@@ -477,7 +383,7 @@ public class TraceVisOverviewChart extends SwingWorker<JPanel, String> {
 		plot.setRenderer(1, exoRender);
 		
 //		add sax boundaries if needed
-		if (normalize && ppa && sax) {
+		if (normalize && sax) {
 			double left = -1;
 			double right = endoMoments.getDomainUpperBound(false)+1;
 			out = buildSAXBoundaries(left, right);
@@ -595,7 +501,7 @@ public class TraceVisOverviewChart extends SwingWorker<JPanel, String> {
             c.insets = new Insets(5,7,5,7);
             this.target.add(flipper, c);
 //            add sax boundaries
-            if (ppa && normalize) {
+            if (normalize) {
             	if (sax) {
             		buttonText = FlipToWOSAX;
             	} else {
